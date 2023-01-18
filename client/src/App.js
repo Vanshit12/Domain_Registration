8ea215e9a6dc61f356df63ccff56b1bb76107e12
vanshit12 <vanshit.muradh@gmail.com>
2023-01-18 22:26:51 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,274 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import {gapi} from 'gapi-script';
+import {Card, Col, Row} from 'react-bootstrap';
+
+function App() {
+
+  var form
+  var chatContainer
+  let loadInterval  
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    var Id = "474219718041-s78lnljsije24b19fk8oafe9jvtg8s7m.apps.googleusercontent.com"
+
+    gapi.load('client:auth2',()=> {
+
+      gapi.auth2.init({clientId: Id})
+    })
+
+    form = document.querySelector('form')
+    chatContainer = document.querySelector('#chat_container')
+    
+    form.addEventListener('submit', handleSubmit)
+    // form.addEventListener('keyup', (e) => {
+    //   e.preventDefault();
+    //   if (e.keyCode === 13) {
+    //       handleSubmit()
+    //   }
+    // })
+
+    },[]);
+
+  const bot = './assets/bot.svg'
+  const user = './assets/user.svg'
+
+  // function loader(element) {
+  //   element.textContent = ''
+
+  //   loadInterval = setInterval(() => {
+  //       // Update the text content of the loading indicator
+  //       element.textContent += '.';
+
+  //       // If the loading indicator has reached three dots, reset it
+  //       if (element.textContent === '....') {
+  //           element.textContent = '';
+  //       }
+  //   }, 300);
+  // }
+
+//   function typeText(element, text) {
+//     let index = 0
+
+//     let interval = setInterval(() => {
+//         if (index < text.length) {
+//             element.innerHTML += text.charAt(index)
+//             index++
+//         } else {
+//             clearInterval(interval)
+//         }
+//     }, 20)
+// }
+// function generateUniqueId() {
+//   const timestamp = Date.now();
+//   const randomNumber = Math.random();
+//   const hexadecimalString = randomNumber.toString(16);
+
+//   return `id-${timestamp}-${hexadecimalString}`;
+// }
+function chatStripe(isAi, value) {
+  return (
+      `
+      <div class="wrapper ai">
+          <div class="chat">
+              <div class="message">Your Business Keyword is like "${value}".</div>
+          </div>
+      </div>
+  `
+  )
+}
+
+function showLogo(value) {
+  return (
+      `
+      <div class="wrappe a">
+          <div class="cha">
+              <img src="${value}" />
+          </div>
+      </div>
+  `
+  )
+}
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form)
+  // user's chatstripe
+  // chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+  // to clear the textarea input 
+  form.reset()
+
+
+  // bot's chatstripe
+  // const uniqueId = generateUniqueId()
+  // chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+  // to focus scroll to the bottom 
+  // chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // specific message div 
+  // const messageDiv = document.getElementById(uniqueId)
+
+  // messageDiv.innerHTML = "..."
+  // loader(messageDiv)
+
+  // const response = await fetch('http://localhost:5000/', {
+  //     method: 'POST',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //         prompt: data.get('prompt')
+  //     })
+  // })
+
+  // axios.post('http://localhost:5000/',JSON.stringify({
+  //             prompt: 'Extract keywords from this text:' + data.get('prompt')
+  //         }),
+  //        {
+  //         headers: {
+  //             'Content-Type': `application/json`,
+  //         }}
+  //     ).then( (res) => {
+  //       chatContainer.innerHTML = chatStripe(false, res.data.keyword)
+  //       console.log(res)
+  //     }).then((res) =>{
+  //         axios.post('http://localhost:5000/logo', JSON.stringify({'logo': res.data.keyword}),
+
+  //         )
+  //         .then((res) => {
+  //           chatContainer.innerHTML += showLogo(res.data.images)
+  //           console.log(res.data)
+  //       })
+  //     })
+
+  var response
+  axios.post('http://localhost:5000/',JSON.stringify({
+              prompt: 'Extract keywords from this text:\n\n' + data.get('prompt')
+          }),
+         {
+          headers: {
+              'Content-Type': `application/json`,
+          }}
+      ).then( (res) => {
+        chatContainer.innerHTML = chatStripe(false, res.data.keyword)
+        console.log("respose from /keywords", res)
+        response = res.data.keyword;
+      }).then((res) =>{
+          axios.post('http://localhost:5000/logo', JSON.stringify({logo: response}),
+          {
+           headers: {
+               'Content-Type': `application/json`,
+           }}
+          )
+          
+          .then((res) => {
+            console.log("respose from logo", res)
+            setImages(res.data.image.data);
+            chatContainer.innerHTML += showLogo(res.data.image.data[0].url)
+            console.log(res.data)
+        })
+      }).then((res) =>{
+        axios.get('https://api.ote-godaddy.com/v1/domains',
+        {
+         headers: {
+             'Content-Type': `application/json`,
+             'Authorization': 'sso-key UzQxLikm_46KxDFnbjN7cQjmw6wocia:46L26ydpkwMaKZV6uVdDWe:2CGjnCYAESzpUcEfWgFcVQ',
+             'Access-Control-Allow-Origin': '*',
+             'Accept': 'application/json'
+         },
+        }).then((res) => {
+          console.log("respose from domains", res)
+          // chatContainer.innerHTML += showLogo(res.data.image.data[0].url)
+          console.log(res.data)
+      })
+    })
+
+
+  // clearInterval(loadInterval)
+  // messageDiv.innerHTML = " "
+
+  // if (response.ok) {
+  //     const data = await response.json();
+  //     const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+  //     typeText(messageDiv, parsedData)
+  // } else {
+  //     const err = await response.text()
+
+  //     messageDiv.innerHTML = "Something went wrong"
+  //     alert(err)
+  // }
+}
+
+const validate = (e) => {
+  console.log(e.target.value);
+  var description = e.target.value;
+  var words = description.split(" ");
+    if (words.length> 10) {
+      alert('Please Describe your business in less than 10 words');
+    }
+}
+
+const handleLogin = async (googleData) => {
+  console.log(googleData);
+  const res = await fetch("http://localhost:5000/auth/google", {
+    method: "POST",
+    body: JSON.stringify({
+      token: googleData.tokenId
+    }),
+  headers: {
+    "Content-Type": "application/json"
+    }
+  })
+  const data = await res.json()
+}
+
+const handleError = async (err) => {
+  console.log(err);
+}
+
+const logout = () => {
+  console.log('logout successfull');
+}
+  return (
+    <div className="App" id="app">
+    <form>
+      <input type="text" id="text" name="prompt" onChange={(e) => validate(e)} placeholder="Please Describe your business in less than 10 words"></input>
+      <button type="submit"><img src="./assets/send.svg" alt="send"></img></button>
+    </form>
+    <div id="chat_container"></div>
+    { console.log(images)}
+    { (images && images.length > 0) &&
+      images.map((img,i) => {
+        return(
+          <Row lg={5} className="g-4">
+            <Col>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={img.url}/>
+              </Card>
+            </Col>
+          </Row>
+        )
+    })
+    }
+    {/* <GoogleLogin
+      clientId={"474219718041-s78lnljsije24b19fk8oafe9jvtg8s7m.apps.googleusercontent.com"}
+      buttonText="Log in with Google"
+      onSuccess={handleLogin}
+      onFailure={handleError}
+      cookiePolicy={'single_host_origin'}
+      prompt={'select_account'}
+    />
+    <GoogleLogout
+      clientId="474219718041-s78lnljsije24b19fk8oafe9jvtg8s7m.apps.googleusercontent.com"
+      buttonText="Logout"
+      onLogoutSuccess={logout}
+      isSignedIn={false}
+    /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,109 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { Configuration, OpenAIApi } from 'openai';
+import { OAuth2Client } from 'google-auth-library';
+
+dotenv.config()
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+var client = new OAuth2Client("474219718041-s78lnljsije24b19fk8oafe9jvtg8s7m.apps.googleusercontent.com")
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/images', express.static('images'))
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: `${prompt}`,
+    //   temperature: 1, // Higher values means the model will take more risks.
+    //   max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    //   top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+    //   frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    //   presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    // });
+
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.5,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.8,
+      presence_penalty: 0.0,
+    });
+
+    res.status(200).send({
+      keyword: response.data.choices[0].text
+    });
+
+      // res.status(200).send({
+      //   keyword: 'Baby Toys'
+      // });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.post('/auth/google', async (req, res) => {
+  const { token }  = req.body
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID
+});
+const { name, email, picture } = ticket.getPayload();
+res.status(200).send({name: name, email: email, dp: picture});
+})
+app.post('/logo', async (req, res) => {
+  try {
+    // const response = await openai.createImage({
+    //   prompt: req.body.logo + "logo",
+    //   n: 5,
+    //   size: "256x256",
+    // });
+      res.status(200).send({image:'response.data'})
+    // const logo = req.body;
+    // res.status(200).send({
+    //   images: path.join("images" +"/baby_care.jpg")
+    // })
+  }
+  catch (err) {
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.post('/domains', async (req, res) => {
+  try {
+    console.log('ok')
+    const response = await fetch("https://api.ote-godaddy.com/v1/domains", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'sso-key UzQxLikm_46KxDFnbjN7cQjmw6wocia:46L26ydpkwMaKZV6uVdDWe'
+      }
+    });
+    res.status(200).send({data:response.data})
+
+  }
+  catch (err) {
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
